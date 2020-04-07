0b9e759de43c2e50b2c18434203ffdf0eea075c8
Adam Kliś <bonus@cert.pl>
2020-04-07 10:04:44 +0200

    PEP 561 compliance, mypy :potega:

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name="msql",
@@ -9,9 +12,11 @@ setup(
     author="CERT Polska",
     author_email="info@cert.pl",
     packages=["msql", "msql.ext"],
+    package_data={"msql": ["py.typed"]},
     license="GPLv3",
     include_package_data=True,
     install_requires=open("requirements.txt").read().splitlines(),
+    zip_safe=False,  # https://mypy.readthedocs.io/en/latest/installed_packages.html#making-pep-561-compatible-packages
     extras_require={
         'pydantic': 'pydantic'
     },
